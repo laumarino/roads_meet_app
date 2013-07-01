35ac286156e7b280a302ad45f4463c2c16d75714
Lauren A. Marino <laumarino@gmail.com>
2013-07-01 16:28:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RoadsMeetApp::Application.config.secret_token = 'de58cf299e2e4a4dd19bc7f45437c59685004e0e88c5269d6066042726fadfb3b21c1e66dee116d957d4cb890f157031e749472b66a25130c2be8a8bf58eb6bc'
